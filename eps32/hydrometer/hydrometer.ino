93fe2d752c94f9f85d226e065d2dc71be477e6a8
canavan-a <aidan.canavan3@gmail.com>
2025-02-07 16:50:05 -0500

    hydrometer code for eps board

---
+++ eps32/hydrometer/hydrometer.ino
@@ -0,0 +1,40 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+#include "config.h" // un and pw
+
+
+WebServer server(80); // Listen on port 80 (HTTP)
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello, ESP32!");
+}
+
+
+void handleHydrometerLevel() {
+  server.send(200, "text/plain", String(analogRead(34)));
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".\n");
+    delay(500);
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/hydrometer", handleHydrometerLevel);
+  server.begin(); // Start server
+}
+
+void loop() {
+  server.handleClient(); // Listen for incoming requests
+
+}
