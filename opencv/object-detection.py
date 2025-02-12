a4f9555706208e81cff26c7958d011c4efb0baa1
canavan-a <aidan.canavan3@gmail.com>
2025-02-11 23:31:37 -0500

    added predict

--- opencv/object-detection.py
+++ opencv/object-detection.py
@@ -49,7 +49,7 @@ while True:
     
     # Perform YOLO detection
     print("frame is about to process")
-    results = model(frame)
+    results = model.predict(frame)
 
     # Draw bounding boxes for cars and people
     for result in results:
