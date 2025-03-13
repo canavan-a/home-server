6a41da88c16782ea0a2f4b4ddb745f8e0e79d1f6
canavan-a <aidan.canavan3@gmail.com>
2025-03-12 22:17:27 -0400

    test opencv on cam

---
+++ mobilenet/linux-test.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import os
+import json
+
+fifo_path = '/tmp/video_pipe2'
+
+
+cap = cv2.VideoCapture(0)  # /dev/video0
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))  # Force MJPG mode
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # Try 1280x720 for balance
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+if not cap.isOpened():
+    print("Error: Could not open video stream.")
+else:
+    print("Video stream is open.")
+
+if not os.path.exists(fifo_path):
+    print("making pipe")
+    os.mkfifo(fifo_path)
+    print("pipe made")
+
+try:
+    print("opening pipe")
+    pipe = open(fifo_path, 'wb')
+    print("pipe open")
+except Exception as e:
+    print(f"Error: Unable to open pipe ({e})")
+    exit(1)
+
+print("starting")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+    pipe.write(frame.tobytes())
+
+
