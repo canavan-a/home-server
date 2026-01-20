6669f53eab57f87989d41e74fde059c20d648e36
canavan-a <aidan.canavan3@gmail.com>
2026-01-20 13:56:55 -0500

    remove sw call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,41 +9,6 @@ import { Plants } from "./pages/Plants";
 import { Clips } from "./pages/Clips";
 import { Garage } from "./pages/Garage";
 
-const registerServiceWorker = () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register(`/sw.js?v=${import.meta.env.BUILD_HASH}`)
-      .then((registration) => {
-        // Listen for updates to the service worker
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          if (installingWorker) {
-            console.log("installing");
-            installingWorker.onstatechange = () => {
-              if (
-                installingWorker.state === "installed" &&
-                navigator.serviceWorker.controller
-              ) {
-                // If there's a new service worker and the page has a controller
-                const userWantsToUpdate = window.confirm(
-                  "A new version of the app is available. Do you want to update?"
-                );
-                if (userWantsToUpdate) {
-                  window.location.reload(); // Reload the page to apply the new service worker
-                }
-              }
-            };
-          }
-        };
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  }
-};
-
-document.addEventListener("visibilitychange", registerServiceWorker);
-
 function App() {
   return (
     <div>
