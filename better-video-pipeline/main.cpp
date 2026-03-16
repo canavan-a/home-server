2019d828ebb5fd11d2c60aea729ab0057f000c1c
canavan-a <aidan.canavan3@gmail.com>
2026-03-15 22:39:14 -0400

    ring buffer cont

--- better-video-pipeline/main.cpp
+++ better-video-pipeline/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <ringbuffer.h>
+#include <memory>
+#include "ringbuffer.h"
+
+struct Frame
+{
+
+    Frame() {}
+};
 
 int main()
 {
+
+    auto f1 = std::make_shared<Frame>();
+
+    auto f2 = std::make_shared<Frame>();
+
+    RingBuffer<std::shared_ptr<Frame>, 10> rb{};
+
+    rb.push(std::move(f1));
+    rb.push(std::move(f2));
 
     return 0;
 }
--- better-video-pipeline/ringbuffer.h
+++ better-video-pipeline/ringbuffer.h
@@ -14,14 +14,68 @@ using Result = std::expected<T, std::string>;
 using Err = std::unexpected<std::string>;
 
 template <typename T, size_t N>
-struct RingBuffer<T, N>
+struct RingBuffer
 {
     std::array<T, N> data{};
 
     int start{};
     int end{};
 
+    int count{};
+
     RingBuffer()
     {
+    }
+
+    void push(const T &item)
+    {
+        this->data[end] = item;
+
+        if ((end) == N - 1)
+        {
+            end = 0;
+        }
+        else
+        {
+            ++end;
+        }
+
+        if (end == start)
+        {
+            if ((start) == N - 1)
+            {
+                start = 0;
+            }
+            else
+            {
+                ++start;
+            }
+        }
+
+        count = std::min((int)N, count + 1);
+    }
+    bool isEmpty()
+    {
+        return count == 0;
+    }
+
+    Result<T> pop()
+    {
+        if (this->isEmpty())
+        {
+            return Err{"buffer is empty"};
+        }
+
+        T item = data[start];
+        if (start == N - 1)
+        {
+            start = 0;
+        }
+        else
+        {
+            ++start;
+        }
+        --count;
+        return item;
     }
 };
