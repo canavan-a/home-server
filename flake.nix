a255042b4999f23a5c82d76ba21a812081265060
canavan-a <aidan.canavan3@gmail.com>
2026-05-18 18:15:57 -0400

    moved libs packages to vendor and system install

--- flake.nix
+++ flake.nix
@@ -5,7 +5,9 @@
         packages.x86_64-linux = {
             server = nixpkgs.legacyPackages.x86_64-linux.callPackage ./server-package.nix {};
             client = nixpkgs.legacyPackages.x86_64-linux.callPackage ./client-package.nix {};
-            streamer = nixpkgs.legacyPackages.x86_64-linux.callPackage ./streamer-package.nix {};
+            streamer = nixpkgs.legacyPackages.x86_64-linux.callPackage ./streamer-package.nix {
+                src = ./.;
+            };
         };
     nixosModules.default = import ./module.nix self;
   };
--- streamer-package.nix
+++ streamer-package.nix
@@ -1,23 +1,19 @@
-{ lib, stdenv, cmake, pkg-config, opencv, gst_all_1, openvino, onnxruntime, fetchgit }:
+{ lib, stdenv, cmake, pkg-config, opencv, gst_all_1, openvino, onnxruntime, cpp-httplib, src }:
 
 stdenv.mkDerivation {
   pname = "streamer";
   version = "0.1.0";
 
-  src = builtins.fetchGit {
-    url = "https://github.com/canavan-a/home-server";
-    ref = "main";
-    rev = "43db89d5d59f7dc68eeac04427bbbad0f143f8d8";
-    submodules = true;
-  };
+  inherit src;
 
-  sourceRoot = "better-video-pipeline";
+  sourceRoot = "${src.name or "source"}/better-video-pipeline";
 
   nativeBuildInputs = [ cmake pkg-config ];
   buildInputs = [
     opencv
     openvino
     onnxruntime
+    cpp-httplib
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
   ];
