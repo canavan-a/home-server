e22eef319e72f311e2c96e2683f34e70984bc5e1
canavan-a <aidan.canavan3@gmail.com>
2026-05-18 20:27:45 -0400

    more gstreamer fixes

--- streamer-package.nix
+++ streamer-package.nix
@@ -1,5 +1,14 @@
 { lib, stdenv, cmake, pkg-config, makeWrapper, opencv, gst_all_1, openvino, onnxruntime, httplib, openssl, src }:
 
+let
+  gstPlugins = [
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+  ];
+  gstPluginPath = lib.makeSearchPath "lib/gstreamer-1.0" gstPlugins;
+in
 stdenv.mkDerivation {
   pname = "streamer";
   version = "0.1.0";
@@ -15,11 +24,7 @@ stdenv.mkDerivation {
     onnxruntime
     httplib
     openssl
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-  ];
+  ] ++ gstPlugins;
 
   installPhase = ''
     mkdir -p $out/bin $out/libexec $out/share/streamer
@@ -28,6 +33,7 @@ stdenv.mkDerivation {
 
     cat > $out/bin/streamer <<EOF
     #!/usr/bin/env bash
+    export GST_PLUGIN_SYSTEM_PATH_1_0="${gstPluginPath}"
     DATA_DIR="\$HOME/streamer"
     mkdir -p "\$DATA_DIR"
     ln -sfn "$out/share/streamer/models" "\$DATA_DIR/models"
