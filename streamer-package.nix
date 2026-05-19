bf9508728797e8b4f167381124924621f4b2ba50
canavan-a <aidan.canavan3@gmail.com>
2026-05-18 20:24:27 -0400

    added more gstreamer plugins

--- streamer-package.nix
+++ streamer-package.nix
@@ -17,6 +17,8 @@ stdenv.mkDerivation {
     openssl
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
   ];
 
   installPhase = ''
