f0224b987add213a366cab5f78528600c00c5e84
canavan-a <aidan.canavan3@gmail.com>
2026-05-20 16:57:33 -0400

    aded multiple options for enabled processes

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Running on lenovo mini PC with Arduino and ESP32 for system hardware controls
 ### Real time camera inference
 
 OpenCV -> OpenVINO(yolo26n) -> Gstreamer(VP8 rtp) -> Pion WebRTC(Go) -> Go relay server + other web controls
-                            -> Arduino Motor Controls
- 
+-> Arduino Motor Controls
+
+### Run with nix pkgs
--- module.nix
+++ module.nix
@@ -1,41 +1,52 @@
 { self }: { config, lib, pkgs, ... }:
 {
   options.services.home-server = {
-    enable = lib.mkEnableOption "home-server";
+    server.enable = lib.mkEnableOption "home-server server process";
+    streamer.enable = lib.mkEnableOption "home-server streamer process";
+    client.enable = lib.mkEnableOption "home-server client process";
+    nginx.enable = lib.mkEnableOption "home-server nginx reverse proxy";
   };
 
-  config = lib.mkIf config.services.home-server.enable {
-    systemd.services.server = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      environment.WEBM_CLIPHOST = "/var/lib/streamer/clips";
-      serviceConfig = {
-        ExecStart = "${self.packages.x86_64-linux.server}/bin/server";
-        Restart = "always";
+  config = lib.mkMerge [
+    (lib.mkIf config.services.home-server.server.enable {
+      systemd.services.server = {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        environment.WEBM_CLIPHOST = "/var/lib/streamer/clips";
+        serviceConfig = {
+          ExecStart = "${self.packages.x86_64-linux.server}/bin/server";
+          Restart = "always";
+        };
       };
-    };
+    })
 
-    systemd.services.streamer = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      serviceConfig = {
-        ExecStart = "${self.packages.x86_64-linux.streamer}/bin/streamer";
-        Restart = "always";
+    (lib.mkIf config.services.home-server.streamer.enable {
+      systemd.services.streamer = {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          ExecStart = "${self.packages.x86_64-linux.streamer}/bin/streamer";
+          Restart = "always";
+        };
       };
-    };
+    })
 
-    systemd.services.client = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      serviceConfig = {
-        ExecStart = "${self.packages.x86_64-linux.client}/bin/client";
-        Restart = "always";
+    (lib.mkIf config.services.home-server.client.enable {
+      systemd.services.client = {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          ExecStart = "${self.packages.x86_64-linux.client}/bin/client";
+          Restart = "always";
+        };
       };
-    };
+    })
 
-    services.nginx = {
-        enable = true;
-        configFile = pkgs.writeText "nginx.conf" (builtins.readFile ./nginx.conf);
-    };
-  };
+    (lib.mkIf config.services.home-server.nginx.enable {
+      services.nginx = {
+          enable = true;
+          configFile = pkgs.writeText "nginx.conf" (builtins.readFile ./nginx.conf);
+      };
+    })
+  ];
 }
