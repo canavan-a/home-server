8f0e59c787f1111784373e87020044b3d84eaf36
= <aidan.canvan3@gmail.com>
2026-05-28 23:23:26 -0400

    claude got the states wrong

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -129,30 +129,16 @@ export const Login = () => {
                 ) : isPending ? (
                   <div className="opacity-40 animate-pulse">
                     {targetStateRef.current === "open" ? (
-                      <svg width="200" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z" fill={LOCK_COLOR} />
-                      </svg>
-                    ) : (
                       <svg width="200px" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M11.5 2C10.6716 2 10 2.67157 10 3.5V6H13V16H1V6H8V3.5C8 1.567 9.567 0 11.5 0C13.433 0 15 1.567 15 3.5V4H13V3.5C13 2.67157 12.3284 2 11.5 2ZM9 10H5V12H9V10Z" fill={LOCK_COLOR} />
+                      </svg>
+                    ) : (
+                      <svg width="200" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z" fill={LOCK_COLOR} />
                       </svg>
                     )}
                   </div>
                 ) : doorState === "open" ? (
-                  <svg
-                    width="200"
-                    height="200px"
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z"
-                      fill={LOCK_COLOR}
-                    />
-                  </svg>
-                ) : doorState === "closed" ? (
                   <svg
                     width="200px"
                     height="200px"
@@ -163,6 +149,20 @@ export const Login = () => {
                       fillRule="evenodd"
                       clipRule="evenodd"
                       d="M11.5 2C10.6716 2 10 2.67157 10 3.5V6H13V16H1V6H8V3.5C8 1.567 9.567 0 11.5 0C13.433 0 15 1.567 15 3.5V4H13V3.5C13 2.67157 12.3284 2 11.5 2ZM9 10H5V12H9V10Z"
+                      fill={LOCK_COLOR}
+                    />
+                  </svg>
+                ) : doorState === "closed" ? (
+                  <svg
+                    width="200"
+                    height="200px"
+                    viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z"
                       fill={LOCK_COLOR}
                     />
                   </svg>
