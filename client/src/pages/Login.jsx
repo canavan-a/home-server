0ac5db155554d22ef23bd60877bc8faa291ffa9e
= <aidan.canvan3@gmail.com>
2026-06-01 17:57:42 -0400

    added better descriptive UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,10 +12,6 @@ export const Login = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [doorState, setDoorState] = useState("unknown");
   const [isToggling, setIsToggling] = useState(false);
-  const [isPending, setIsPending] = useState(false);
-  const preClickStateRef = useRef(null);
-  const targetStateRef = useRef(null);
-  const pendingTimeoutRef = useRef(null);
 
   useEffect(() => {
     const pw = localStorage.getItem("pw");
@@ -29,13 +25,7 @@ export const Login = () => {
     axios
       .get(`https://aidan.house/api/door/state?doorCode=${pw}`)
       .then((response) => {
-        const newState = response.data.state;
-        setDoorState(newState);
-        if (preClickStateRef.current !== null && newState !== preClickStateRef.current) {
-          preClickStateRef.current = null;
-          clearTimeout(pendingTimeoutRef.current);
-          setIsPending(false);
-        }
+        setDoorState(response.data.state);
       })
       .catch(() => {});
   };
@@ -48,34 +38,17 @@ export const Login = () => {
     return () => clearInterval(interval);
   }, [loggedIn, password]);
 
-  const doToggle = () => {
-    if (isPending) return;
-
-    const endpoint =
-      doorState === "open"
-        ? "https://aidan.house/api/door/close"
-        : "https://aidan.house/api/door/open";
-
-    preClickStateRef.current = doorState;
-    targetStateRef.current = doorState === "open" ? "closed" : "open";
+  const doCommand = (action) => {
+    if (isToggling) return;
+    const endpoint = action === "open"
+      ? "https://aidan.house/api/door/open"
+      : "https://aidan.house/api/door/close";
     setIsToggling(true);
-
     axios
       .post(endpoint, { doorCode: password })
-      .then(() => {
-        setIsToggling(false);
-        clearTimeout(pendingTimeoutRef.current);
-        pendingTimeoutRef.current = setTimeout(() => {
-          preClickStateRef.current = null;
-          targetStateRef.current = null;
-          setIsPending(false);
-        }, 15000);
-        setIsPending(true);
-      })
+      .then(() => setIsToggling(false))
       .catch(() => {
         alert("could not send command");
-        preClickStateRef.current = null;
-        targetStateRef.current = null;
         setIsToggling(false);
       });
   };
@@ -120,55 +93,36 @@ export const Login = () => {
         <div className="w-full max-w-md p-4">
           <div className="grid gap-4">
             {loggedIn ? (
-              <div
-                onClick={doToggle}
-                className="text-center w-full flex flex-col items-center justify-center space-y-4"
-              >
+              <div className="text-center w-full flex flex-col items-center justify-center space-y-6">
                 {isToggling ? (
                   <span className="loading loading-infinity loading-lg"></span>
-                ) : isPending ? (
-                  <div className="opacity-40 animate-pulse">
-                    {targetStateRef.current === "open" ? (
-                      <svg width="200px" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M11.5 2C10.6716 2 10 2.67157 10 3.5V6H13V16H1V6H8V3.5C8 1.567 9.567 0 11.5 0C13.433 0 15 1.567 15 3.5V4H13V3.5C13 2.67157 12.3284 2 11.5 2ZM9 10H5V12H9V10Z" fill={LOCK_COLOR} />
-                      </svg>
-                    ) : (
-                      <svg width="200" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z" fill={LOCK_COLOR} />
-                      </svg>
-                    )}
-                  </div>
                 ) : doorState === "open" ? (
-                  <svg
-                    width="200px"
-                    height="200px"
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M11.5 2C10.6716 2 10 2.67157 10 3.5V6H13V16H1V6H8V3.5C8 1.567 9.567 0 11.5 0C13.433 0 15 1.567 15 3.5V4H13V3.5C13 2.67157 12.3284 2 11.5 2ZM9 10H5V12H9V10Z"
-                      fill={LOCK_COLOR}
-                    />
+                  <svg width="200px" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M11.5 2C10.6716 2 10 2.67157 10 3.5V6H13V16H1V6H8V3.5C8 1.567 9.567 0 11.5 0C13.433 0 15 1.567 15 3.5V4H13V3.5C13 2.67157 12.3284 2 11.5 2ZM9 10H5V12H9V10Z" fill={LOCK_COLOR} />
                   </svg>
                 ) : doorState === "closed" ? (
-                  <svg
-                    width="200"
-                    height="200px"
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z"
-                      fill={LOCK_COLOR}
-                    />
+                  <svg width="200" height="200px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M4 6V4C4 1.79086 5.79086 0 8 0C10.2091 0 12 1.79086 12 4V6H14V16H2V6H4ZM6 4C6 2.89543 6.89543 2 8 2C9.10457 2 10 2.89543 10 4V6H6V4ZM7 13V9H9V13H7Z" fill={LOCK_COLOR} />
                   </svg>
                 ) : (
                   <span className="loading loading-infinity loading-lg"></span>
                 )}
+                <div className="flex gap-4 w-full">
+                  <button
+                    onClick={() => doCommand("open")}
+                    disabled={isToggling || doorState === "open"}
+                    className="btn btn-lg btn-glass flex-1"
+                  >
+                    Unlock
+                  </button>
+                  <button
+                    onClick={() => doCommand("close")}
+                    disabled={isToggling || doorState === "closed"}
+                    className="btn btn-lg btn-glass flex-1"
+                  >
+                    Lock
+                  </button>
+                </div>
               </div>
             ) : (
               <>
